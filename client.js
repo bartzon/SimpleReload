6e2b3f0da9c64add64c123020460285fab19aa9d
Bart Zonneveld <bart@madebyhiro.com>
2013-04-13 13:44:20 +0200

    Added support for turbolinks and pjax

--- client.js
+++ client.js
@@ -1,9 +1,18 @@
 var socket = io.connect('http://RELOAD_HOST:RELOAD_PORT');
 
 socket.on('connect', function() {
+  console.log("Connected to SimpleReload")
 })
 
 window.addEventListener('load', function() {
+  socket.emit('url', document.location.href)
+})
+
+document.addEventListener('page:change', function() {
+  socket.emit('url', document.location.href)
+})
+
+document.addEventListener('pjax:end', function() {
   socket.emit('url', document.location.href)
 })
 
